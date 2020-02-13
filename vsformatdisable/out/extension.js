ffbe0b3595bc136325f42e9a43fd302dfbdce703
Nick Bueltel <nick.bueltel@trintech.com>
2020-02-13 10:21:21 -0600

    update to toggle

--- vsformatdisable/out/extension.js
+++ vsformatdisable/out/extension.js
@@ -5,18 +5,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = require("vscode");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+var activatedState = false;
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vsformatdisable" is now active!');
+    console.log('Congratulations, your extension "testextension" is now active!');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.disable', () => {
+    let toggleAll = vscode.commands.registerCommand('extension.toggleAll', () => {
+        var _a;
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
+        if (activatedState === false) {
+            vscode.window.showInformationMessage('DEACTIVATED auto-formatting');
+            const test = vscode.workspace.getConfiguration('vsformatdisable');
+            (_a = test.inspect("formatOnSave")) === null || _a === void 0 ? void 0 : _a.globalValue;
+            vscode.workspace.getConfiguration().update("editor.formatOnSave", false, true);
+            activatedState = true;
+        }
+        else {
+            vscode.workspace.getConfiguration().update("editor.formatOnSave", true, true);
+            vscode.window.showInformationMessage('ACTIVATED auto-formatting');
+            activatedState = false;
+        }
     });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(toggleAll);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
--- vsformatdisable/src/extension.ts
+++ vsformatdisable/src/extension.ts
@@ -1,27 +1,36 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
+import { format } from 'path';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+var activatedState = false;
 export function activate(context: vscode.ExtensionContext) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "testextension" is now active!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let toggleAll = vscode.commands.registerCommand('extension.toggleAll', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        if (activatedState === false) {
+            vscode.window.showInformationMessage('DEACTIVATED auto-formatting');
+            const test = vscode.workspace.getConfiguration('vsformatdisable');
+            test.inspect("formatOnSave")?.globalValue;
+            vscode.workspace.getConfiguration().update("editor.formatOnSave", false, true);
+            activatedState = true;
+        }
+        else {
+            vscode.workspace.getConfiguration().update("editor.formatOnSave", true, true);
+            vscode.window.showInformationMessage('ACTIVATED auto-formatting');
+            activatedState = false;
+        }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vsformatdisable" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.disable', () => {
-		// The code you place here will be executed every time your command is executed
-		
-		// Display a message box to the user
-		
-	});
-
-	context.subscriptions.push(disposable);
+    });
+    context.subscriptions.push(toggleAll);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
